03edc7b090c3a37c224222bd5c5f7a77dfbcb6b9
Haleem Hamid <hhamidbiz@gmail.com>
2022-01-03 16:52:31 -0800

    v-1

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fe
+let list = document.querySelector("#list")
+
+//fect all teams
+  function fetchTeams() {
+      fetch("http://localhost:3000/data")
+      .then((res) => res.json())
+      .then((teams) => {
+        teams.forEach(element => {
+          sideNav(element)
+        });
+      });
 }
+
+//append to side nav
+function sideNav(teams){
+const li = document.createElement("li")
+li.innerText = teams.name
+list.appendChild(li)
+}
+
+fetchTeams()
