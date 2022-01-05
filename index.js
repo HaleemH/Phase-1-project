f13ba0761cb2511264bd2e30241532ebb7664d66
Haleem Hamid <hhamidbiz@gmail.com>
2022-01-05 09:50:43 -0800

    Updated search to display json data / removed table

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-let list = document.querySelector("#list")
-let engine = document.querySelector(".form")
+let list = document.querySelector("#list");
+let engine = document.querySelector(".form");
 let searchInput;
+//formIds
+const fName = document.querySelector("#teamName"); //.innerText = element.name;
+const fCity = document.querySelector("#teamCity"); //.innerText = element.city;
+const fConf = document.querySelector("#teamConference"); //.innerText = element.conference;
+const fDiv = document.querySelector("#teamDivision"); //.innerText = element.division;
+const display = document.querySelector("#display");
 
-function init(){
-  fetchTeams()
-  createSearchFunction()
+function init() {
+  fetchTeams();
+  createSearchFunction();
 }
 
 //fetch all teams
-  function fetchTeams() {
-      fetch("http://localhost:3000/data")
-      .then((res) => res.json())
-      .then((teams) => {
-          console.log(teams);
-          teams.forEach(element => {
-          sideNav(element)
-        });
+function fetchTeams() {
+  fetch("http://localhost:3000/data")
+    .then((res) => res.json())
+    .then((teams) => {
+      // console.log(teams);
+      teams.forEach((element) => {
+        sideNav(element);
       });
+    });
 }
 
 //establish eventlistener
 //check to see if input matches teamname, if so display it
-function createSearchFunction(){
-  engine.addEventListener('submit', handleSubmit);
+function createSearchFunction() {
+  engine.addEventListener("submit", handleSubmit);
 }
 
-function handleSubmit(e){
-    e.preventDefault();
-    searchInput = document.querySelector("#name").value;
-    console.log(searchInput);
+function handleSubmit(e) {
+  e.preventDefault();
+  searchInput = document.querySelector("#name").value;
+  console.log(searchInput);
 
-    //check teams and display...need to change html elements here
-    fetch("http://localhost:3000/data")
-      .then((res) => res.json())
-      .then((teams) => {
-          teams.forEach(element => {
-            if(element.name === searchInput){
-              document.querySelector("#teamName").innerText = element.name;
-              document.querySelector("#teamCity").innerText = element.city;
-              document.querySelector("#teamConference").innerText = element.conference;
-              document.querySelector("#teamDivision").innerText = element.division;
-              document.querySelector("#teamImg").src = element.img;
-            }
-          });
-        });
+  //check teams and display...need to change html elements here
+  fetch("http://localhost:3000/data")
+    .then((res) => res.json())
+    .then((teams) => {
+      teams.forEach((element) => {
+        if (element.name.toUpperCase() === searchInput.toUpperCase()) {
+          const img = document.createElement("img");
+          img.src = element.img;
+          display.prepend(img);
+          const name = element.name;
+          fName.append(name);
+          const city = element.city;
+          fCity.append(city);
+          const conf = element.conference;
+          fConf.append(conf);
+          const div = element.division;
+          fDiv.append(div);
+        }
+      });
+    });
 }
 
 //append to side nav
-function sideNav(teams){
-  const li = document.createElement("li")
-  li.innerText = teams.name
-  list.appendChild(li)
+function sideNav(teams) {
+  const li = document.createElement("li");
+  li.innerText = teams.name;
+  list.appendChild(li);
+
+  //click event on list items
+  const clicked = li.addEventListener("click", (e) => {
+    const click = e.target;
+    console.log(click);
+  });
 }
